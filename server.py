2bfdf8156b0d4ef2acda2eecfe1c81e4d81749d4
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-09-04 22:32:45 +0300

    add server

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 12345
+server_running = True
+client_num = 0
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)
+    
+def handle_client(client_socket, client_address):
+    global server_running
+    global client_num
+    client_num += 1
+    print(f"Connection from {client_address}")
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        message = data.decode()
+        print(f"Received from {client_address}: {message}")
+        if message.lower() == 'exit':
+            print(f"Client {client_address} disconnected")
+            client_num -= 1
+            if client_num == 0:
+                server_running = False
+            break
+        else:
+            client_socket.send(data) 
+    client_socket.close()
+    
+
+
+try:
+    while server_running:
+        client_socket, client_address = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
+finally:
+    server_socket.close()
+
+    
