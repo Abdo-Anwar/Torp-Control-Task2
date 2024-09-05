9686316d0149ce241f25f3fbde311fd5c8382d70
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-09-05 19:31:22 +0300

    modified thclient to send the required command

--- client.py
+++ client.py
@@ -1,18 +1,21 @@
-import socket as sk
-#local ip = '192.168.1.15'
-#port = 12345
-ip = input("Enter ip address: ")
-PORT = int(input("Enter sever port: "))
+import socket
 
+ip = input("Enter server IP address: ")
+PORT = int(input("Enter server port: "))
 
-client_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-client_socket.connect((ip,PORT))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((ip, PORT))
 
 while True:
-    data= input("Enter Data or 'exit' to exit: ")
-    client_socket.sendall(data.encode())  
-    if data.lower() == 'exit':
-        break
-    receivedData=client_socket.recv(1024) 
-    print(f"Data received from server is {receivedData.decode()}")
-client_socket.close() 
+    command = input('Enter "get" to retrieve files, "post" to upload, or "exit" to exit: ').lower()
+    
+    if command == "get" or command == "post" or command == "exit":
+        client_socket.send(command.encode())
+        if command == 'exit':
+            break
+        response = client_socket.recv(1024).decode()
+        print(f"Server response: {response}")
+    else:
+        print("Invalid input. Please try again.")
+
+client_socket.close()
