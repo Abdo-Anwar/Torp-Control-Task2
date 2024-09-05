e6e7db23652f2b51b79a9a9fb33bf11570e9597f
Anwar <abd.ahm.anwar@gmail.com>
2024-09-05 14:20:38 +0300

    add run func and finish get and post funcs

--- httpCode/serverhttp.py
+++ httpCode/serverhttp.py
@@ -9,9 +9,12 @@ UPLOAD_DIR = 'Files_Uploads'
 if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
 
+
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         file_path = self.path.lstrip('/')
+        #full_file_path = os.path.join(UPLOAD_DIR, file_path)
 
         if file_path == '':  #  no path specified)
                 self.send_response(200)
@@ -30,3 +33,37 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write(b'404 Not Found')
+
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        post_params = parse_qs(post_data)
+
+         # Save the uploaded file (assuming form field is 'file')
+        file_content = post_params.get('file', [None])[0]
+        if file_content:
+            file_path = os.path.join(UPLOAD_DIR, "uploaded_file.txt")  # Save to UPLOAD_DIR
+            with open(file_path, "w") as f:
+                f.write(file_content)
+            
+            # Respond with acknowledgment
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"File uploaded successfully!")
+        else:
+            # No file found in the POST request
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write(b"Bad Request: No file uploaded.")
+
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port) # '' The empty string means the server will listen on all available IP addresses
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
