34d5a272049da573935a485bd9f529b88c905cb6
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-09-05 19:32:22 +0300

    modified server to handle the given commands

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from httpCode import serverhttp as st
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 12345
@@ -9,29 +10,36 @@ client_num = 0
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(5)
-    
+
 def handle_client(client_socket, client_address):
     global server_running
     global client_num
     client_num += 1
     print(f"Connection from {client_address}")
+    
     while True:
-        data = client_socket.recv(1024)
+        data = client_socket.recv(1024).decode()
+        print(data)
         if not data:
             break
-        message = data.decode()
-        if message.lower() == 'exit':
+        
+        if data.lower() == 'exit':
             print(f"Client {client_address} disconnected")
             client_num -= 1
             if client_num == 0:
                 server_running = False
             break
+        elif data.lower() == 'get':
+            handler = st.SimpleHTTPRequestHandler(client_socket, client_address, None)
+            handler.do_GET()
+        elif data.lower() == 'post':
+            handler = st.SimpleHTTPRequestHandler(client_socket, client_address, None)
+            handler.do_POST()
         else:
-            print(f"Received from {client_address}: {message}")
-            client_socket.send(data) 
+            client_socket.send(b"Invalid command")
+    
     client_socket.close()
     
-
 
 try:
     while server_running:
@@ -40,5 +48,3 @@ try:
         client_thread.start()
 finally:
     server_socket.close()
-
-    
